99c13dd84de49ca77208e6eae1257fba1b28d817
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2023-12-12 02:05:38 +0500

    Task-49 Добавление исключений

--- Seminar-08/Task-49.py
+++ Seminar-08/Task-49.py
@@ -15,18 +15,67 @@ from os.path import exists
 from csv import DictReader, DictWriter
 
 
+class LenNumberError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+class NameError(Exception):         # Объявляем исключение
+    def __init__(self, txt):
+        self.txt = txt
+
+
 def GetInfo():
-    firstName = 'Иван'
-    lastName = 'Иванов'
-    phoneNumber = '89531585239'
+    isValidFirstName = False
+    while not isValidFirstName:
+        try:                                        # Выполняем проверку на исключения
+            firstName = input('Введите имя')
+            if len(firstName) < 2:
+                raise NameError('Невалидное имя')   # Выбрасываем исключение
+            else:
+                isValidFirstName = True
+
+        except NameError as err:                    # Отлавливаем исключение
+            print(err)                              # Обработка исключения
+            continue
+
+    isValidLastName = False
+    while not isValidLastName:
+        try:
+            lastName = input('Введите Фамилию')
+            if len(lastName) < 2:
+                raise NameError('Невалидная фамилия')
+            else:
+                isValidLastName = True
+
+        except NameError as err:
+            print(err)
+            continue
+
+    isValidPhone = False
+    while not isValidPhone:
+        try:
+            phoneNumber = int(input('Введите номер'))
+            if len(str(phoneNumber)) != 11:
+                raise LenNumberError('Неверная длинна номера')
+            else:
+                isValidPhone = True
+
+        except ValueError:
+            print('Не валидный номер')
+            continue
+        except LenNumberError as err:
+            print(err)
+            continue
+
     return [firstName, lastName, phoneNumber]
 
 
 def CreateFile(fileName):
-    # Менеджер контекста
+    # with open() - Менеджер контекста
     with open(fileName, 'w', encoding='utf-8') as data:
         fWriter = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
-        fWriter.writeheader()  # Записываем заголовок
+        fWriter.writeheader()   # Записываем заголовок
 
 
 def ReadFile(fileName):
@@ -36,13 +85,21 @@ def ReadFile(fileName):
 
 
 def WriteFile(fileName, list1):
-    res = ReadFile(fileName)  # создаём словарь (res) в который читаем содержимое файла
+    res = ReadFile(fileName)    # создаём словарь (res) в который читаем содержимое файла
+
+    for elem in res:                            # Выполняем проверку на совпадение
+        if elem["Телефон"] == list1[2]:         # вводимого телефона с уже существующими
+            print('Такой телефон уже есть')
+            return                              # Полностью выходим из функции и начинаем
+                                                # всё заново
+
+
     obj = {"Имя": list1[0], "Фамилия": list1[1], "Телефон": list1[2]}
-    res.append(obj)  # дополняем словарь-res
+    res.append(obj)                         # дополняем словарь-res
     with open(fileName, 'w', encoding='utf-8', newline='') as data:
         fWriter = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
-        fWriter.writeheader()  # Записываем заголовок
-        fWriter.writerows(res)  # передаём в файл список со словарями
+        fWriter.writeheader()               # Записываем заголовок
+        fWriter.writerows(res)              # передаём в файл список со словарями
 
 
 fileName = 'phone.csv'
@@ -51,16 +108,19 @@ fileName = 'phone.csv'
 def main():
     while True:
         command = input('Введите комманду: ')
-        if command == 'q':
+        if command == 'q':                  # Выход из программы
             break
-        elif command == 'w':
-            if not exists(fileName):
-                CreateFile(fileName)
+
+        elif command == 'w':                # Запись данных
+            if not exists(fileName):        # Проверка наличия файла
+                CreateFile(fileName)        # Если нет - создаём новый
             WriteFile(fileName, GetInfo())
-        elif command == 'r':
-            if not exists(fileName):
-                print('файл отсутствует')
+
+        elif command == 'r':                # Чтение данных
+            if not exists(fileName):        # Проверка наличия файла
+                print('файл отсутствует')   # Если нет - возвращаемся в главное меню
                 continue
             print(*ReadFile(fileName))
+
 
 main()
